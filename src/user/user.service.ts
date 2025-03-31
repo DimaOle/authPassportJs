53a6aea211eab0731c371f890fd2c5e40b9d55f9
Dima Olekseenko <dimkaolekseenko@gmail.com>
2025-03-31 09:03:39 +0300

    fixed prisma service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    
+    constructor(private readonly prismaService: PrismaService) { }
+    
+    save(user: Partial<User>) { 
+        return this.prismaService.user.create({
+            data: {
+                email: user.email,
+                password: user.password,
+                roles: ["USER"],
+            }}
+        )
+    }
+
+
+    
+    findOne() { }
+
+    delete() { }
+}
