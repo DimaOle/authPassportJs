10b7257e2f91f62b77c4e0a3bf09532e7e3f300c
Dima Olekseenko <dimkaolekseenko@gmail.com>
2025-04-01 16:00:46 +0300

    add decorators password matching

---
+++ libs/common/src/decarators/is-passwords-matching-constraint.decorator.ts
@@ -0,0 +1,15 @@
+import { RegisterDto } from "@auth/dto";
+import { ValidationArguments, ValidatorConstraint, ValidatorConstraintInterface } from "class-validator";
+
+
+@ValidatorConstraint({ name: 'IsPasswordMatching', async: false })
+export class IsPasswordMatchingConstraint implements ValidatorConstraintInterface {
+    validate(passwordRepeat: string, args: ValidationArguments) {
+        const obj = args.object as RegisterDto 
+        return obj.password === passwordRepeat
+    }
+
+    defaultMessage(validationArguments?: ValidationArguments): string {
+        return 'The passwords do not match'
+    }
+}
