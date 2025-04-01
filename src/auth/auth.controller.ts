08966edcb98871d831c7b6a57c15fd9bef4d629e
Dima Olekseenko <dimkaolekseenko@gmail.com>
2025-04-01 17:40:02 +0300

    add login auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,16 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LoginDto, RegisterDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
-  register(@Body() dto) { }
+  register(@Body() dto: RegisterDto) { }
   
   @Post('login')
-  login(@Body() dto) { }
+  login(@Body() dto: LoginDto) { }
   
   @Get('refresh')
   refreshTokin() {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Token } from '@prisma/client';
+import { PrismaService } from '@prisma/prisma.service';
+import { UserService } from '@user/user.service';
+import { compareSync } from 'bcrypt';
+import { add } from 'date-fns';
+import { v4 } from 'uuid';
+import { LoginDto, RegisterDto } from './dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    private readonly logger = new Logger(AuthService.name)
+    constructor(
+        private readonly userService: UserService,
+        private readonly jwtService: JwtService,
+        private readonly prismaServise: PrismaService
+    ) {}
+    
+    async register(dto: RegisterDto) {
+        return this.userService.save(dto).catch(err => {
+            this.logger.error(err)
+            return null
+        })
+    }
+
+    async login(dto: LoginDto) {
+        const user = await this.userService.findOne(dto.email).catch(err => {
+            this.logger.error(err)
+            return null
+        })
+
+        if (!user || !compareSync(dto.password, user.password)) {
+            throw new UnauthorizedException("Incorrectly password or email")
+        }
+
+        const accessToken = this.jwtService.sign({
+            id: user.id,
+            email: user.email,
+            roles: user.roles
+        })
+
+        const refreshToken = await this.getRefreshToken(user.id)
+        
+        return {accessToken, refreshToken}
+    }
+
+    private async getRefreshToken(userId: string): Promise<Token> {
+        return this.prismaServise.token.create({
+            data: {
+                token: v4(),
+                exp: add(new Date(), { months: 1 }),
+                userId,
+            }
+        })
+    }
+}
